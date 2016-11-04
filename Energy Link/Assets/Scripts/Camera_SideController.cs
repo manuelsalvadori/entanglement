c8ab5f0cf7562a84102919224b227bb226a46849
Matteo Marras <matteo.marras1994@gmail.com>
2016-11-04 16:28:07 +0100

    Added 3D Scroll Side view for the Linking Experiment

---
+++ Energy Link/Assets/Scripts/Camera_SideController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_SideController : MonoBehaviour {
+
+    public GameObject m_pl1;
+    public GameObject m_pl2;
+
+    private Vector3 m_offset;
+    private Vector3 distance;
+    private Vector3 median;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        median = new Vector3(m_pl1.transform.position.x, (m_pl1.transform.position.y + m_pl2.transform.position.y) / 2, (m_pl1.transform.position.z + m_pl2.transform.position.z) / 2);
+        m_offset = transform.position - median;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        median = new Vector3(m_pl1.transform.position.x, (m_pl1.transform.position.y + m_pl2.transform.position.y) / 2, (m_pl1.transform.position.z + m_pl2.transform.position.z) / 2);
+        transform.position = median + m_offset;
+    }
+}
