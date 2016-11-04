307c3ef6d70cf35b11de689d6846798908df0881
Matteo Marras <matteo.marras1994@gmail.com>
2016-11-04 15:49:32 +0100

    Inserted an experiment. Link between 2 characters controlled at the same time by the Player. Using Lightning Bolt Effect by Jeff Johnson ( https://www.assetstore.unity3d.com/en/#!/content/59471)

---
+++ Energy Link/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Range(10f, 200f)]
+    public float m_speed = 50f;
+
+    [Range(10f, 200f)]
+    public float m_jump_quantity = 50f;
+
+    private float m_jump = 0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>() as Rigidbody;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_jump = m_jump_quantity;
+        }
+
+        float m_horizontal = Input.GetAxis("Horizontal");
+        float m_vertical = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(m_horizontal * m_speed, m_jump*m_speed, m_vertical * m_speed));
+        m_jump = 0f;
+    }
+}
