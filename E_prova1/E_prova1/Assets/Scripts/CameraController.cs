d81793bf6b40b91e37b1228058503ffe0f6d461f
Matteo Marras <matteo.marras1994@gmail.com>
2016-11-11 13:50:51 +0100

    Camera Rotation along a Spline! PAWAAAA

---
+++ E_prova1/E_prova1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject m_target;
+
+    private Mesh m_mesh;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        //
+    }
+}
