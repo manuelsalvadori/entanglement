63a4c277086916dab7e1ec7bb4a1f9b8eadbe680
Matteo Marras <matteo.marras1994@gmail.com>
2016-11-16 17:38:37 +0100

    Added inventory and item classes for manages items and upgrades

---
+++ E_prova1/E_prova1/Assets/Scripts/Invetory.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Invetory : MonoBehaviour {
+
+    private bool[] m_upgrades;
+    private List<Item> m_items;
+    private int m_ncollectables = 0;
+
+    void Awake()
+    {
+        m_upgrades = new bool[4] { false, false, false, false };
+        m_items = new List<Item>();
+        m_ncollectables = 0;
+    }
+
+	void addItem(Item i)
+    {
+        m_items.Add(i);
+    }
+
+    List<Item> getItems()
+    {
+        return m_items;
+    }
+
+    Item takeItem(int n)
+    {
+        if (n < m_items.Count)
+        {
+            Item tmp = m_items.ToArray()[n];
+            m_items.RemoveAt(n);
+            return tmp;
+        }
+        return null;
+    }
+
+    void score()
+    {
+        m_ncollectables++;
+    }
+
+    int getScore()
+    {
+        return m_ncollectables;
+    }
+
+    bool hasUpgrade(int n)
+    {
+        if(n < 4)
+        {
+            return m_upgrades[n];
+        }
+        return false;
+    }
+
+    bool[] getUpgrades() { return m_upgrades; }
+
+
+
+
+}
---
+++ E_prova1/E_prova1/Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item
+{
+
+    private string m_name;
+    public GameObject m_this;
+    public GameObject m_target;
+
+    public bool testTarget(GameObject other)
+    {
+        return m_target == other;
+    }
+}
