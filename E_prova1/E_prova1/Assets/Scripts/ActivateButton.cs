55a8f24c5c1e0f0b4998aeb0a48aceae19fb301a
Manuel Salvadori <manuelsalvadori@MacBook-Pro-di-Manuel.local>
2016-11-16 23:29:50 +0100

    added button script

---
+++ E_prova1/E_prova1/Assets/Scripts/ActivateButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateButton : MonoBehaviour {
+
+
+    public bool m_isActive = false;
+
+	void Update ()
+    {
+        if ((transform.position - (GameManager.Instance.m_players[(GameManager.Instance.m_sel_pg) ? 0 : 1]).transform.position).magnitude < 5f)
+        {
+            displayActive();
+
+            if (Input.GetButtonDown("Interact"))
+            {
+                m_isActive = true;
+            }
+        }
+	}
+
+    void displayActive()
+    {
+        //facciamo apparire una UI
+    }
+}
