fea97f94df436d5f1b1282098bd7698a10cde385
Manuel Salvadori <manuelsalvadori@cln-188-79.eduroam.unimi.it>
2016-11-17 15:08:27 +0100

    added tag over button

--- E_prova1/E_prova1/Assets/Scripts/ActivateButton.cs
+++ E_prova1/E_prova1/Assets/Scripts/ActivateButton.cs
@@ -6,36 +6,55 @@ public class ActivateButton : MonoBehaviour {
 
     public bool m_isActive = false;
     public bool m_isSwitch = false;
+    public GameObject square;
 
 	void Update ()
     {
         
-        if ((transform.position - (GameManager.Instance.m_players[(GameManager.Instance.m_sel_pg) ? 0 : 1]).transform.position).magnitude < 5f)
+        if ((transform.position - (GameManager.Instance.m_players[(GameManager.Instance.m_sel_pg) ? 0 : 1]).transform.position).magnitude < 4f)
         {
-            displayActive();
+            if(!(!m_isSwitch && m_isActive))
+                displayActive();
 
             if (Input.GetButtonDown("Interact"))
             {
                 if (m_isSwitch)
                     m_isActive = !m_isActive;
                 else
+                {
                     m_isActive = true;
+                    displayDeactive();
+                }
             }
         }
+        else
+        {
+            square.SetActive(false);
+        }
+
 	}
 
     void displayActive()
     {
         if (/*Input.GetJoystickNames()[0].Equals("Sony PLAYSTATION(R)3 Controller")*/  Input.GetJoystickNames().Length > 0)
         {
-            Debug.Log("ps3");
+            square.SetActive(true);
         }
         else
         {
             Debug.Log("keyboard");
-
         }
+    }
 
-
+    void displayDeactive()
+    {
+        if (/*Input.GetJoystickNames()[0].Equals("Sony PLAYSTATION(R)3 Controller")*/  Input.GetJoystickNames().Length > 0)
+        {
+            square.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("keyboard");
+        }
     }
 }
