e4633d1b3d7e4694c24b1248d66eba270f01b171
Manuel Salvadori <manuelsalvadori@cln-156-129.eduroam.unimi.it>
2016-11-17 11:04:14 +0100

    bugfix button script

--- E_prova1/E_prova1/Assets/Scripts/ActivateButton.cs
+++ E_prova1/E_prova1/Assets/Scripts/ActivateButton.cs
@@ -9,6 +9,7 @@ public class ActivateButton : MonoBehaviour {
 
 	void Update ()
     {
+        
         if ((transform.position - (GameManager.Instance.m_players[(GameManager.Instance.m_sel_pg) ? 0 : 1]).transform.position).magnitude < 5f)
         {
             displayActive();
@@ -25,7 +26,7 @@ public class ActivateButton : MonoBehaviour {
 
     void displayActive()
     {
-        if (Input.GetJoystickNames()[0].Equals("Sony PLAYSTATION(R)3 Controller"))
+        if (/*Input.GetJoystickNames()[0].Equals("Sony PLAYSTATION(R)3 Controller")*/  Input.GetJoystickNames().Length > 0)
         {
             Debug.Log("ps3");
         }
