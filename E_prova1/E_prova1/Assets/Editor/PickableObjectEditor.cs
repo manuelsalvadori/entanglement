5f06e7eadcfe54d6fbf5c3417ce0bb082f9f9937
Manuel Salvadori <manuelsalvadori@MacBook-Pro-di-Manuel.local>
2016-11-21 21:30:29 +0100

    resolved building conflict

---
+++ E_prova1/E_prova1/Assets/Editor/PickableObjectEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(PickableObject))]
+[CanEditMultipleObjects]
+public class PickableObjectEditor : Editor
+{
+    override public void OnInspectorGUI()
+    {
+        var myScript = target as PickableObject;
+
+
+        myScript.isCollectable = GUILayout.Toggle(myScript.isCollectable, "Is Collectable");
+
+
+        if (!myScript.isCollectable)
+        {
+            myScript.isUpgrade = GUILayout.Toggle(myScript.isUpgrade, "Is Upgrade");
+            if (myScript.isUpgrade)
+                myScript.m_gadget = (Inventory.Gadgets)EditorGUILayout.EnumPopup("Which Gadget:", myScript.m_gadget);
+            else
+            {
+                myScript.m_target = (GameObject)EditorGUILayout.ObjectField("Target", myScript.m_target, typeof(GameObject), true);
+                myScript.m_spriteTmp = (Sprite)EditorGUILayout.ObjectField("Inventory View", myScript.m_spriteTmp, typeof(Sprite), true, new GUILayoutOption[] { GUILayout.MaxHeight(15), GUILayout.MinWidth(100) });
+            }
+        }
+        else
+        {
+            myScript.isUpgrade = false;
+        }
+
+        if (GUILayout.Button("Reset"))
+        {
+            myScript.isCollectable = false;
+            myScript.isUpgrade = false;
+            myScript.m_target = null;
+            myScript.m_spriteTmp = null;
+        }
+    }
+}
--- E_prova1/E_prova1/Assets/Scripts/PickableObject.cs
+++ E_prova1/E_prova1/Assets/Scripts/PickableObject.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEditor;
 using System.Collections;
 
 public class PickableObject : MonoBehaviour{
@@ -35,42 +34,4 @@ public class PickableObject : MonoBehaviour{
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 
-}
-
-[CustomEditor(typeof(PickableObject))]
-[CanEditMultipleObjects]
-public class PickableObjectEditor : Editor
-{
-    override public void OnInspectorGUI()
-    {
-        var myScript = target as PickableObject;
-
-
-        myScript.isCollectable = GUILayout.Toggle(myScript.isCollectable, "Is Collectable");
-
-
-        if (!myScript.isCollectable)
-        {
-            myScript.isUpgrade = GUILayout.Toggle(myScript.isUpgrade, "Is Upgrade");
-            if (myScript.isUpgrade)
-                myScript.m_gadget = (Inventory.Gadgets)EditorGUILayout.EnumPopup("Which Gadget:", myScript.m_gadget);
-            else
-            {
-                myScript.m_target = (GameObject)EditorGUILayout.ObjectField("Target", myScript.m_target, typeof(GameObject), true);
-                myScript.m_spriteTmp = (Sprite)EditorGUILayout.ObjectField("Inventory View", myScript.m_spriteTmp, typeof(Sprite), true, new GUILayoutOption[] { GUILayout.MaxHeight(15), GUILayout.MinWidth(100) });
-            }
-        }
-        else
-        {
-            myScript.isUpgrade = false;
-        }
-
-        if (GUILayout.Button("Reset"))
-        {
-            myScript.isCollectable = false;
-            myScript.isUpgrade = false;
-            myScript.m_target = null;
-            myScript.m_spriteTmp = null;
-        }
-    }
 }
