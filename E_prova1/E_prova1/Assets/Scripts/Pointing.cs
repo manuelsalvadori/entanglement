8e81be3577e4a74a7ad68a38779514c5b30ab94d
Manuel Salvadori <manuelsalvadori@MacBook-Pro-di-Manuel.local>
2016-11-22 14:13:34 +0100

    finally teleport is done :'D

--- E_prova1/E_prova1/Assets/Scripts/Pointing.cs
+++ E_prova1/E_prova1/Assets/Scripts/Pointing.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Pointing : MonoBehaviour
 {
     public float speed = 8f;
-    public float[] pos_y = {10f, -2f};
+    public float[] start_ray_y = {10f, -2f};
     LineRenderer lr;
     public Material[] mat;
 
@@ -23,13 +23,12 @@ public class Pointing : MonoBehaviour
         lr.SetPosition(0, transform.position);
         lr.SetPosition(1, GameManager.Instance.m_players[GameManager.Instance.m_sel_pg ? 0:1].transform.position);
         RaycastHit hit;
-        Vector3 start = new Vector3(transform.position.x, pos_y[(GameManager.Instance.m_sel_pg) ? 0 : 1], transform.position.z);
+        Vector3 start = new Vector3(transform.position.x, start_ray_y[(GameManager.Instance.m_sel_pg) ? 0 : 1], transform.position.z);
         Ray raggio = new Ray(start, Vector3.down);
         Physics.Raycast(raggio, out hit);
 
         if (hit.collider != null)
         {
-            Debug.Log("collido");
             transform.position = new Vector3(transform.position.x, (hit.collider.bounds.max.y + gameObject.GetComponent<MeshRenderer>().bounds.extents.y),transform.position.z);
         }
            
@@ -48,16 +47,22 @@ public class Pointing : MonoBehaviour
         float ha = (GameManager.Instance.m_inventoryIsOpen) ? 0 : Input.GetAxis("R_Horizontal");
 
         RaycastHit hit;
-        Vector3 start = new Vector3(transform.position.x, pos_y[(GameManager.Instance.m_sel_pg) ? 0 : 1], transform.position.z);
+        Vector3 start = new Vector3(transform.position.x, start_ray_y[(GameManager.Instance.m_sel_pg) ? 0 : 1], transform.position.z);
         Ray raggio = new Ray(start, Vector3.down);
         Physics.Raycast(raggio, out hit);
 
-        if (hit.collider != null)
-        {
-            transform.position = new Vector3(transform.position.x, (hit.collider.bounds.max.y + gameObject.GetComponent<MeshRenderer>().bounds.extents.y),transform.position.z);
-        }
-
         transform.position += (new Vector3(ha, 0f, va).normalized * Time.deltaTime * speed);
+        if (!hit.collider.tag.Equals("Barriera"))
+        {
+            Debug.Log(hit.collider.tag);
+
+            transform.position = new Vector3(transform.position.x, (hit.collider.bounds.max.y + gameObject.GetComponent<MeshRenderer>().bounds.extents.y), transform.position.z);
+        }
+        else
+        {
+            Debug.Log(hit.collider.tag);
+            transform.position = new Vector3(hit.collider.bounds.min.x - gameObject.GetComponent<MeshRenderer>().bounds.extents.x, transform.position.y, transform.position.z);
+        }
 
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos = Camera.main.ViewportToWorldPoint(new Vector3(Mathf.Clamp(pos.x, 0.03f, 0.97f), pos.y, pos.z));
--- E_prova1/E_prova1/Assets/Scripts/SwitchGadget.cs
+++ E_prova1/E_prova1/Assets/Scripts/SwitchGadget.cs
@@ -19,8 +19,11 @@ public class SwitchGadget : MonoBehaviour
         {
             m_state++;
             m_state = (m_state > 2) ? 0 : m_state;
-            if(m_state != 1)
+            if (m_state != 1)
+            {
+                GameManager.Instance.m_players[GameManager.Instance.m_sel_pg ? 0 : 1].GetComponent<PlayerController>().teleportAllowed = false;
                 switchMirino(false);
+            }
 
             transform.GetChild(m_state).GetComponent<Animation>()["RigthToCenter"].speed = 1f;
             transform.GetChild(m_state).GetComponent<Animation>()["RigthToCenter"].time = 0f;
@@ -40,8 +43,10 @@ public class SwitchGadget : MonoBehaviour
             m_state--;
             m_state = (m_state < 0) ? 2 : m_state;
             if(m_state != 1)
+            {
+                GameManager.Instance.m_players[GameManager.Instance.m_sel_pg ? 0 : 1].GetComponent<PlayerController>().teleportAllowed = false;
                 switchMirino(false);
-            
+            }
             int m_state1 = (m_state - 1 < 0) ? m_state + 2 : m_state -1;
             int m_state2 = (m_state - 2 < 0) ? ((m_state - 2 == -1)? 2 : 1) : m_state - 2;
 
