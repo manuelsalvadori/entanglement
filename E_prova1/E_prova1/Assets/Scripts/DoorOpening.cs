862e7771cf00d1000651e7c417f980b3a6605154
Manuel Salvadori <morke.bm@hotmail.it>
2016-12-02 14:36:54 +0100

    fixed dooropening

--- E_prova1/E_prova1/Assets/Scripts/DoorOpening.cs
+++ E_prova1/E_prova1/Assets/Scripts/DoorOpening.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorOpening : MonoBehaviour {
-
+public class DoorOpening : MonoBehaviour
+{
+    public int m_door_number;
 	public Transform m_endPosition;
 	public Transform m_initPosition;
-	public ActivateButton m_button;
+    public ActivateButton[] m_button;
 	private Vector3 target = Vector3.zero;
 	private Vector3 velocity;
 	public bool active = true;
@@ -20,11 +21,29 @@ public class DoorOpening : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
-		if (m_button != null)
-		{
-			Debug.Log("bottone!");
-			active = m_button.m_isActive;
-		}
+        switch (m_door_number)
+        {
+            case 1:
+                active = m_button[1].m_isActive || m_button[3].m_isActive;
+                break;
+            case 2:
+                active = m_button[2].m_isActive || m_button[3].m_isActive || m_button[6].m_isActive;
+                break;
+            case 3:
+                active = m_button[4].m_isActive || m_button[5].m_isActive;
+                break;
+            case 4:
+                active = m_button[2].m_isActive || m_button[4].m_isActive;
+                break;
+            case 5:
+                active = m_button[1].m_isActive || m_button[5].m_isActive;
+                break;
+            case 6:
+                active = m_button[1].m_isActive || m_button[2].m_isActive || m_button[3].m_isActive || m_button[6].m_isActive;
+                break;
+            default:
+                return;
+        }
 
 		if ((transform.position - target).magnitude < 0.3f)
 		{
