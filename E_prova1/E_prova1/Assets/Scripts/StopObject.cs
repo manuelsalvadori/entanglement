f0e9785139520979caa8b155468c6707127d9808
Manuel Salvadori <morke.bm@hotmail.it>
2016-12-02 11:32:50 +0100

    added stopObject script

---
+++ E_prova1/E_prova1/Assets/Scripts/StopObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopObject : MonoBehaviour
+{
+
+    public bool m_isActive = false;
+
+    void OnCollisionExit(Collision col)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        if (m_isActive)
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+    }
+}
