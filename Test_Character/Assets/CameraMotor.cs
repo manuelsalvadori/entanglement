e90f7e24c914cb873c075ad74adaeee8ca719c7f
Matteo Marras <matteo.marras1994@gmail.com>
2016-12-08 23:51:31 +0100

    Character Controller Study -> Jump and Movement Done
    Task Completed:
    - Animation
    - Move Forward
    - Turn
    - Move on Air
    
    TO DO LIST:
    - Player Falling (Algorithm already designed)
    - Player Link (Almost done)

---
+++ Test_Character/Assets/CameraMotor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMotor : MonoBehaviour {
+
+	public GameObject target;
+	public float distance;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position - new Vector3 (0, 0, distance);
+	}
+}
