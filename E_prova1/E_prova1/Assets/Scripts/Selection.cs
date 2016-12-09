1d8f7141a48d29a45a867b55e55b483737298723
Manuel Salvadori <morke.bm@hotmail.it>
2016-12-09 14:54:34 +0100

    Added scene manager

---
+++ E_prova1/E_prova1/Assets/Scripts/Selection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Selection : MonoBehaviour
+{
+
+    public void scene1()
+    {
+        SceneManager.LoadScene("Microlivello_1");
+    }
+
+    public void scene2()
+    {
+        SceneManager.LoadScene("Microlivello_2");
+    }
+
+    public void scene3()
+    {
+        SceneManager.LoadScene("Microlivello_3");
+    }
+
+    public void scene4()
+    {
+        SceneManager.LoadScene("Microlivello_4");
+    }
+
+    public void scene5()
+    {
+        SceneManager.LoadScene("Microlivello_5");
+    }
+
+    public void scene6()
+    {
+        SceneManager.LoadScene("Microlivello_6");
+    }
+}
