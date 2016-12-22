774bb949598f33b89b9cd28a10cc4fe7b97917c0
Manuel Salvadori <morke.bm@hotmail.it>
2016-12-22 12:15:56 +0100

    update level 0

---
+++ E_prova1/E_prova1/Assets/Scripts/drone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drone : MonoBehaviour
+{
+    public float speed = 1f;
+    float init_y;
+	
+    void Start()
+    {
+        init_y = transform.position.y;
+    }
+
+	void Update ()
+    {
+        transform.position = new Vector3(transform.position.x, init_y + Mathf.Sin(Time.time*speed)/3f, transform.position.z);
+	}
+}
