6cf655577403a4e47014c51403a8a05cb308e587
Manuel Salvadori <morke.bm@hotmail.it>
2016-12-27 18:17:51 +0100

    updated level 0 - colliders problems :/

---
+++ E_prova1/E_prova1/Assets/Scripts/pushBoxes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushBoxes : MonoBehaviour
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionStay(Collision o)
+    {
+        if (o.gameObject.CompareTag("Player1"))
+        {
+            Debug.Log(o.gameObject.name);
+        }
+    }
+}
