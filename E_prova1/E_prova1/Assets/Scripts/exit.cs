40f591bc4dace782bd923fcf21f24125a246716b
Manuel Salvadori <morke.bm@hotmail.it>
2016-12-28 14:10:16 +0100

    updated level 0 - complete more or less

---
+++ E_prova1/E_prova1/Assets/Scripts/exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exit : MonoBehaviour {
+
+    void OnTriggerEnter(Collider o)
+    {
+        SceneManager.LoadScene("LevelSelection");
+    }
+}
