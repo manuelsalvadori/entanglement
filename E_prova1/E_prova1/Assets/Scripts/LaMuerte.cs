60e57ad419967062379b34a3ee1d55c17699d49d
Matteo Marras <matteo.marras1994@gmail.com>
2017-01-03 03:43:58 +0100

    Test LaMuerte, da completare

---
+++ E_prova1/E_prova1/Assets/Scripts/LaMuerte.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaMuerte : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    public Texture2D [] frames;
+    float framesPerSecond = 120f;
+
+    // Update is called once per frame
+    void Update () {
+        int index = (int)(Time.time * framesPerSecond) % frames.Length;
+
+        GetComponent<Renderer>().material.mainTexture = frames[index];
+    }
+}
