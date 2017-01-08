e958db46e118c78ae0872ecaea18c2d44bd39a45
Alberto Ronchetti <happyalby@hotmail.it>
2017-01-08 14:10:28 +0100

    fixed automatic door script for door_key movement

--- E_prova1/E_prova1/Assets/Scripts/AutomaticDoors.cs
+++ E_prova1/E_prova1/Assets/Scripts/AutomaticDoors.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AutomaticDoors : MonoBehaviour
 {
-    public Transform door_left, door_right;
+    public Transform door_left, door_right, door_key;
     public Transform player;
     public float distance = 2f;
     bool isclosed = true;
@@ -36,6 +36,7 @@ public class AutomaticDoors : MonoBehaviour
 
         Vector3 currentpos = door_right.localPosition;
         Vector3 currentpos2 = door_left.localPosition;
+		Vector3 currentpos3 = door_key.localPosition;
 
         float counter = 0;
         while (counter < durations)
@@ -45,11 +46,13 @@ public class AutomaticDoors : MonoBehaviour
             {
                 door_right.localPosition = Vector3.Lerp(currentpos, currentpos + new Vector3(1.85f, 0f, 0f), counter / durations);
                 door_left.localPosition = Vector3.Lerp(currentpos2, currentpos2 - new Vector3(1.85f, 0f, 0f), counter / durations);
+				door_key.localPosition = Vector3.Lerp(currentpos3, currentpos3 - new Vector3(1.85f, 0f, 0f), counter / durations);
             }
             else
             {
                 door_right.localPosition = Vector3.Lerp(currentpos, currentpos - new Vector3(1.85f, 0f, 0f), counter / durations);
                 door_left.localPosition = Vector3.Lerp(currentpos2, currentpos2 + new Vector3(1.85f, 0f, 0f), counter / durations);
+				door_key.localPosition = Vector3.Lerp(currentpos3, currentpos3 + new Vector3(1.85f, 0f, 0f), counter / durations);
             }
             yield return null;
         }
