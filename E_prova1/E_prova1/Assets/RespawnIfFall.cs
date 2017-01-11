b9defb88abbc6cd227aaea9fe59e87e86bc28ae3
Matteo Marras <matteo.marras1994@gmail.com>
2017-01-11 17:58:43 +0100

    Boxes respawn script

---
+++ E_prova1/E_prova1/Assets/RespawnIfFall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnIfFall : MonoBehaviour {
+
+    private Vector3 birth_Position;
+    public GameObject respawn;
+
+	// Use this for initialization
+	void Start () {
+        birth_Position = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetInstanceID() == respawn.GetInstanceID())
+        {
+            transform.position = birth_Position;
+        }
+    }
+}
