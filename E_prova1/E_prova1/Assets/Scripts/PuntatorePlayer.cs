a39b72481357d2e85ad112877bb9c1964495b151
Manuel Salvadori <morke.bm@hotmail.it>
2017-01-13 17:52:55 +0100

    added manually box colliders to the 2 labyrinth and removed players exagon marks in 3d mode

--- E_prova1/E_prova1/Assets/Scripts/PuntatorePlayer.cs
+++ E_prova1/E_prova1/Assets/Scripts/PuntatorePlayer.cs
@@ -8,26 +8,38 @@ public class PuntatorePlayer : MonoBehaviour
     public float speed = 50f;
     private float[] start_ray_y = {40f, -20f};
     public bool p1 = true;
+    int layermask = 1 << 8;
+    Vector3 start;
+    MeshRenderer mr;
 
-	// Update is called once per frame
+    void Awake()
+    {
+        mr = GetComponent<MeshRenderer>();
+        layermask = ~layermask;
+    }
+
 	void Update ()
     {
-        transform.Rotate(0f, Time.deltaTime * speed, 0f, Space.World);
-        transform.position = GameManager.Instance.m_players[p1 ? 1 : 0].transform.position;
-
-        RaycastHit hit;
-        Vector3 start = new Vector3(transform.position.x, start_ray_y[p1 ? 0 : 1], transform.position.z);
-        Ray raggio = new Ray(start, Vector3.down);
-
-        int layermask = 1 << 8;
-        layermask = ~layermask;
-
-        Physics.Raycast(raggio, out hit, Mathf.Infinity, layermask);
-
-        if (hit.collider != null)
+        if (GameManager.Instance.m_3D_mode)
         {
-            //transform.position = new Vector3(transform.position.x, (hit.collider.bounds.max.y + gameObject.GetComponent<MeshRenderer>().bounds.extents.y) + 0.01f, transform.position.z);
-            transform.position = new Vector3(transform.position.x, hit.point.y + 0.1f, transform.position.z);
+            mr.enabled = false;
+        }
+        else
+        {
+            mr.enabled = true;
+            transform.Rotate(0f, Time.deltaTime * speed, 0f, Space.World);
+            transform.position = GameManager.Instance.m_players[p1 ? 1 : 0].transform.position;
+
+            RaycastHit hit;
+            start = new Vector3(transform.position.x, GameManager.Instance.m_players[!p1 ? 1 : 0].transform.position.y + 1f, transform.position.z);
+            Ray raggio = new Ray(start, Vector3.down);
+
+            Physics.Raycast(raggio, out hit, Mathf.Infinity, layermask);
+
+            if (hit.collider != null)
+            {
+                transform.position = new Vector3(transform.position.x, hit.point.y + 0.1f, transform.position.z);
+            }
         }
     }
 }
