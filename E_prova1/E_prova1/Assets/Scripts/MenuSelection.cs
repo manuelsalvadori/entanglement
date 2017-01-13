c3688b6f1c88efdff4997cc17348a85ba0bbc876
Manuel Salvadori <morke.bm@hotmail.it>
2017-01-13 22:08:04 +0100

    fixed main menu for joypad selection

---
+++ E_prova1/E_prova1/Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuSelection : MonoBehaviour {
+
+    public EventSystem es;
+    public GameObject go;
+
+    private bool buttonSelected;
+
+	void Start ()
+    {
+        Cursor.visible = false;
+        es.SetSelectedGameObject(go);
+	}
+	
+	void Update ()
+    {
+        if ((Input.GetAxisRaw("Vertical") != 0) && buttonSelected == false)
+        {
+            es.SetSelectedGameObject(go);
+            buttonSelected = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
