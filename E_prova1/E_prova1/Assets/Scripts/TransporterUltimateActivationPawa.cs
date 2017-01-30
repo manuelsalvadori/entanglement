4f378b772d44383ade2d708defa1f7a8b6d22648
Manuel Salvadori <morke.bm@hotmail.it>
2017-01-30 16:19:32 +0100

    boh non mi viene in mente niente

---
+++ E_prova1/E_prova1/Assets/Scripts/TransporterUltimateActivationPawa.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransporterUltimateActivationPawa : MonoBehaviour
+{
+    public GameObject mightyLightOfBluishPowa;
+    public GameObject mightyLightOfReddishPowa;
+
+    void OnTriggerEnter(Collider o)
+    {
+        if (o.CompareTag("Player1"))
+        {
+            mightyLightOfBluishPowa.SetActive(true);
+        }
+
+        if (o.CompareTag("Player2"))
+        {
+            mightyLightOfReddishPowa.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider o)
+    {
+        if (o.CompareTag("Player1"))
+        {
+            mightyLightOfBluishPowa.SetActive(false);
+        }
+
+        if (o.CompareTag("Player2"))
+        {
+            mightyLightOfReddishPowa.SetActive(false);
+        }
+    }
+}
