1cad29100df2578c08985e6475ced2261bd27c60
Manuel Salvadori <morke.bm@hotmail.it>
2017-01-31 22:17:59 +0100

    updated upgrades GUI

---
+++ E_prova1/E_prova1/Assets/changeGUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeGUI : MonoBehaviour
+{
+    public Image guir, guib;
+    public Sprite spred, spblue;
+
+    void OnTriggerEnter()
+    {
+        guir.sprite = spred;
+        guib.sprite = spblue;
+        Destroy(this);
+    }
+
+}
